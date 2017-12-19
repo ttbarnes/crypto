5e63f673e9ac49fba9ff9d7ed040d5a0b90dcb14
ttbarnes <tony@tonybarnes.me>
2017-12-19 18:30:00 +0000

    balances poc

--- src/App.js
+++ src/App.js
@@ -2,10 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      bitfinexData: []
+    }
+  }
+  onInputChange = (ev) => {
+    this.setState({
+      inputValue: ev.target.value
+    });
+  }
+  
+  componentDidMount(){
+    if (!this.state.bitfinexData.length) {
+      fetch('http://localhost:8080/api/poc').then(res => res.json()).then((data) => {
+        this.setState({
+          bitfinexData: data
+        });
+      });
+    }
+  }
+
   render() {
+    const { bitfinexData } = this.state;
+
     return (
       <div className="App">
         <h1>Crypto app</h1>
+        {/* <input type="text" onChange={this.onInputChange} placeholder="asdf" /> */}
+
+        {bitfinexData.length ? (
+          <div>
+            <h4>Bitfinex Balances</h4>
+            <ul>
+              {bitfinexData.map((d) =>
+                <li key={d.currency}>
+                  <b>{d.currency.toUpperCase()}</b>: {d.amount}
+                </li>
+              )}
+            </ul>
+          </div>
+        ) : null}
       </div>
     );
   }
