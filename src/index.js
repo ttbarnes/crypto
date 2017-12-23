92bb7b110d20e756de522dffe0773beb790abb1e
ttbarnes <tony@tonybarnes.me>
2017-12-23 11:36:30 +0000

    signup links, route

--- src/index.js
+++ src/index.js
@@ -4,22 +4,29 @@ import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 import './index.css';
 import Home from './Home';
 import MoveFunds from './MoveFunds';
+import SignUp from './routes/SignUp/SignUp';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <BrowserRouter>
       <div>
         <header>
-          <h1><Link to="/">iCryptoWallet64.js.io</Link></h1>
-          <ul>
-            <li><Link to="/">Balances</Link></li>
-            <li><Link to="move-funds">Move funds</Link></li>
-          </ul>
+          <div className="col-left">
+            <h1><Link to="/">iCryptoWallet64.js.io</Link></h1>
+            <ul>
+              <li><Link to="/">Balances</Link></li>
+              <li><Link to="move-funds">Move funds</Link></li>
+            </ul>
+          </div>
+          <div className="user-menu">
+            <li><Link to="sign-up">Sign up</Link></li>
+          </div>
         </header>
         <div className="main-container">
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/move-funds' component={MoveFunds} />
+            <Route exact path='/sign-up' component={SignUp} />
           </Switch>
         </div>
       </div>
---
+++ src/routes/SignUp/SignUp.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //   }
+  // }
+
+  render() {
+    return (
+      <div>
+        <h4>Sign up</h4>
+      </div>
+    )
+  }
+
+}
+
+export default SignUp;
